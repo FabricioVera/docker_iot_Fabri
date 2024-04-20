fe0a96ad840620339c0e8c8e782d0927b5c67335
FabricioVera <fabriciovera7@gmail.com>
2024-04-20 17:57:34 -0300

    Mensajes leidos en colas y clase contador creada

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -1,30 +1,56 @@
 import asyncio, ssl, certifi, logging, os, sys
 import aiomqtt
 
-logging.basicConfig(format='%(asctime)s - cliente mqtt - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
+logging.basicConfig(format='%(funcname)s - %(asctime)s - cliente mqtt - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
 
-global cont
-cont = 0
+class CONTADOR:
+    def __init__(self):
+        self.contador = 0
+    def incrementar(self):
+        self.contador += 1
 
-async def publicar_contador(client):
-    global cont
-    while True:
-        await asyncio.sleep(5)
-        await client.publish(os.environ['TOPICOCONTADOR'], payload=cont)
 
-async def contador():
-    global cont
+async def contar():
     while True:
         await asyncio.sleep(3)
-        cont=cont+1
-        logging.info(cont)
+        await CONTADOR.incrementar()
 
-async def leer_topicos(client):
+
+async def publicar_contador(client):
+    while True:
+        await asyncio.sleep(5)
+        await client.publish(os.environ['TOPICOCONTADOR'], payload=CONTADOR.contador)
+
+async def lectura_topicoa():
+    while True:
+        message = await topicoa_queue.get()
+        logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+
+
+async def lectura_topicob():
+    while True:
+        message = await topicob_queue.get()
+        logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+
+#crear las colas para los mensajes
+topicoa_queue = asyncio.Queue()
+topicob_queue = asyncio.Queue()
+
+async def distributor(client):
+    # Ordenar los mensajes recibidos en sus respectivas colas
+    async for message in client.messages:
+        if message.topic.matches("TOPICOA"):
+            topicoa_queue.put_nowait(message)
+        elif message.topic.matches("TOPICOA"):
+            topicob_queue.put_nowait(message)
+
+
+"""async def leer_topicos(client):
     async for message in client.messages:
             if message.topic.matches(os.environ['TOPICOA']):
                 logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
             if message.topic.matches(os.environ['TOPICOB']):
-                logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+                logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))"""
 
 async def main():
     tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
@@ -44,9 +70,11 @@ async def main():
         await client.subscribe(os.environ['TOPICOB'])
         #await publicar_contador(client)
         async with asyncio.TaskGroup() as tg:
-            tg.create_task(contador())
             tg.create_task(publicar_contador(client))
-            tg.create_task(leer_topicos(client))
+            tg.create_task(distributor(client))
+            tg.create_task(lectura_topicoa())
+            tg.create_task(lectura_topicob())
+            tg.create_task(contar())
         
             
 

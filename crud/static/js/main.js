a24d91b02a959659fac355ab65a18d18c268cd4a
FabricioVera <fabriciovera7@gmail.com>
2024-06-11 15:42:38 -0300

    se puso el tema por defecto el tema claro

--- crud/static/js/main.js
+++ crud/static/js/main.js
@@ -44,7 +44,7 @@ function changetheme(themestate){
     setCookie("theme", themestate, 1);
   }
   else{
-    document.getElementById('estilos').setAttribute("href",`https://bootswatch.com/5/darkly/bootstrap.min.css`);
+    document.getElementById('estilos').setAttribute("href",`https://bootswatch.com/5/cosmo/bootstrap.min.css`);
   }
 }
   
